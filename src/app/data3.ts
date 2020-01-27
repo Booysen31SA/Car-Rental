74e118dbbb309383948d7d971da713086d071714
Matthew Booysen <MatthewBoo@awards.co.za>
2020-01-27 15:50:06 +0200

    updated line graph

--- src/app/data3.ts
+++ src/app/data3.ts
@@ -4,15 +4,51 @@ export var single = [
     "series": [
       {
         "name": "Jan",
-        "value": 62000000
+        "value": 62
       },
       {
         "name": "Feb",
-        "value": 73000000
+        "value": 73
       },
       {
         "name": "Mar",
-        "value": 89400000
+        "value": 23
+      },
+      {
+        "name": "Apr",
+        "value": 56
+      },
+      {
+        "name": "May",
+        "value": 28
+      },
+      {
+        "name": "Jun",
+        "value": 90
+      },
+      {
+        "name": "Jul",
+        "value": 60
+      },
+      {
+        "name": "Aug",
+        "value": 5
+      },
+      {
+        "name": "Sep",
+        "value": 21
+      },
+      {
+        "name": "Oct",
+        "value": 80
+      },
+      {
+        "name": "Nov",
+        "value": 100
+      },
+      {
+        "name": "Dec",
+        "value": 10
       }
     ]
   }
